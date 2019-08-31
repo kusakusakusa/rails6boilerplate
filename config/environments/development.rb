9d21b85b06403712139e178f882e28a9952690c5
Vic-L <vljc17@gmail.com>
2019-08-30 21:49:17 -0300

    chore: letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # for sending devise emails with default url_options
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
