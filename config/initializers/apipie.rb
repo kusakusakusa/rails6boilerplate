9210d4822c94960cbdfa34a85effe3dded76b28a
Vic-L <vljc17@gmail.com>
2019-09-05 13:05:49 -0300

    chore: change api doc url

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Apipie.configure do |config|
-  config.app_name = 'Rails6boilerplate'
+  config.app_name = Rails.application.class.module_parent_name
   config.api_base_url['v1'] = '/api/v1'
   # config.api_base_url['v2'] = '/api/v2'
-  config.doc_base_url = '/apipie'
+  config.doc_base_url = '/apidoc'
   config.default_version = 'v1'
   config.translate = false
   # where is your API defined?
