c4c67f8d4decbe01abc3ffe03edb70e019cb8c07
Victor Leong <vljc17@gmail.com>
2019-10-14 16:56:02 +0800

    fix: use dig to get rails credentials

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -370,7 +370,7 @@ Doorkeeper::JWT.configure do
 
   # Set the encryption secret. This would be shared with any other applications
   # that should be able to read the payload of the token. Defaults to "secret".
-  secret_key Rails.application.credentials.jwt[:secret]
+  secret_key Rails.application.credentials.dig(:jwt, :secret)
 
   # If you want to use RS* encoding specify the path to the RSA key to use for
   # signing. If you specify a `secret_key_path` it will be used instead of
