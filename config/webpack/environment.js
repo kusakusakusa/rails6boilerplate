dd59c15fdc905ec3f2be7ae03a7570483fc79d6c
Victor Leong <vljc17@gmail.com>
2019-10-26 15:36:05 +0800

    chore: remove unnecessary import rule

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,12 +9,5 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Chart: 'chart.js',
 }))
 
-// taking reference from https://gist.github.com/jrunestone/2fbe5d6d5e425b7c046168b6d6e74e95#file-jquery-datatables-webpack
-// read more about the AMD and commonjs https://addyosmani.com/writing-modular-js/
-environment.loaders.append('import', {
-  test: /^datatables\.net.*(?<!css)$/,
-  loader: 'imports-loader'
-})
-
 environment.loaders.prepend('erb', erb)
 module.exports = environment
