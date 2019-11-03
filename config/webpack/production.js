5e5b792128a3b4dd7e1469c0f1a059ddb6fd992c
Victor Leong <vljc17@gmail.com>
2019-11-03 22:49:19 +0800

    Revert "chore: use md5 to ensure the content hash of generated assets are the same in all servers"
    This reverts commit 4257fc700eae83a67e531f07f40ec5ffbc7bce25.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,15 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-// with reference to https://github.com/rails/webpacker/issues/1215#issuecomment-360604583
-// use md5 to generate css and js files with same hash
-// other methods not working so far
-const WebpackMd5Hash = require('webpack-md5-hash')
-environment.plugins.append(
-  'WebpackMd5Hash',
-  new WebpackMd5Hash()
-)
-environment.config.set('output.filename', '[name]-[chunkhash].js')
-environment.config.set('devtool', 'sourcemap')
-
 module.exports = environment.toWebpackConfig()
