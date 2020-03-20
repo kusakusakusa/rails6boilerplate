89cc325b6b3d0bdc62e0cfec5215e9e44e357f4c
Vic-L <vljc17@gmail.com>
2020-03-20 16:38:43 +0800

    fix: remove chart.js  as part of provideplugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,7 @@ const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
-  Popper: ['popper.js', 'default'],
-  Chart: 'chart.js',
+  Popper: ['popper.js', 'default']
 }))
 
 environment.loaders.prepend('erb', erb)
