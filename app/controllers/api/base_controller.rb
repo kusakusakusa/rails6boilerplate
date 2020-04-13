3d7330f903866ec4cd9ed1c8473be59a77eaf7f3
Vic-L <vljc17@gmail.com>
2020-04-13 23:25:39 +0800

    feat: use metaprogramming to create current_resource methods for multiple users in API environment

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -9,12 +9,24 @@ module Api
 
     private
 
-    def current_user
-      @current_user ||= if doorkeeper_token
-                          User.find(doorkeeper_token.resource_owner_id)
-                        else
-                          warden.authenticate(scope: :user, store: false)
-                        end
+    class_eval do
+      Devise.mappings.keys.each do |resource|
+        define_method :"current_#{resource.to_s}" do
+          if eval("@current_#{resource.to_s}.nil?")
+            eval("@current_#{resource.to_s}.nil?")
+            instance_variable_set(
+              "@current_#{resource.to_s}", 
+              if doorkeeper_token
+                eval("#{resource.to_s.camelcase}.find(doorkeeper_token.resource_owner_id)")
+              else
+                warden.authenticate(scope: resource, store: false)
+              end
+            )
+          else
+            eval("@current_#{resource.to_s}")
+          end
+        end
+      end
     end
   end
 end
