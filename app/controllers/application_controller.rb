7151ec59ff367ec92aa4c7114a69ec40a617f640
Vic-L <vljc17@gmail.com>
2020-04-18 01:28:31 +0800

    fix: set fix after_sign_in_path for cms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || cms_root_path
+    cms_root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
