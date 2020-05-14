b1e145475ef0149a10dff208032cab99b7b6c52c
Vic-L <vljc17@gmail.com>
2020-05-14 10:19:40 +0800

    chore: use mailinator.com for user

--- db/seeds/0_users.rb
+++ db/seeds/0_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 p 'Creating users'
-['user1@test.com', 'user2@test.com'].each do |email|
+['user1@mailinator.com', 'user2@mailinator.com'].each do |email|
   User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
