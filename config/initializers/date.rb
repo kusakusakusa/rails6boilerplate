bb156fd97908d3ab141f4a5aeab9a5783f8c96cb
Vic-L <vljc17@gmail.com>
2020-06-07 11:13:57 +0800

    test: helper test

--- config/initializers/date.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# TODO test
-
-class Date
-  def display
-    self&.strftime('%b %d, %Y') || '-'
-  end
-end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ApplicationHelper do
+  feature '#display_date' do
+    scenario 'should return "-" if date is nil' do
+      expect(helper.display_date(nil)).to eq '-'
+    end
+
+    scenario 'should return date in format' do
+      binding.pry
+      expect(helper.display_date(Date.parse('2020-12-31'))).to eq "Dec 31, 2020"
+    end
+  end
+end
