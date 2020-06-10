ab2e017695c978cb11c4d8f1c147f9fb0680650d
Vic-L <vljc17@gmail.com>
2020-06-10 22:01:42 +0800

    feat: add natural sort to datatables

--- app/javascript/packs/datatables.js
+++ app/javascript/packs/datatables.js
@@ -1,5 +1,6 @@
 require("datatables.net")
 require('datatables.net-bs4')
+require('datatables.net-plugins/sorting/natural')
 require("datatables.net-bs4/css/dataTables.bootstrap4.min.css")
 
 const dataTables = [];
@@ -8,7 +9,10 @@ document.addEventListener("turbolinks:load", () => {
   if (dataTables.length === 0 && $('.data-table').length !== 0) {
     $('.data-table').each((_, element) => {
       dataTables.push($(element).DataTable({
-        pageLength: 50
+        pageLength: 50,
+        columnDefs: [
+          { type: 'natural-nohtml', targets: '_all' }
+        ]
       }));
     });
   }
