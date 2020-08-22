a7c605497187f46b6528bcb263cf057f1fbd7238
Vic-L <vljc17@gmail.com>
2020-08-22 18:49:02 +0800

    feat: remove timeoutable as not v favorable among clients

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -20,5 +20,5 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable, :timeoutable
+         :recoverable, :rememberable, :validatable
 end
