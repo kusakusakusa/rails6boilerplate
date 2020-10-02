95a22d3794f48bcc38bcc2d976ef3c9934925560
Vic-L <vljc17@gmail.com>
2020-10-02 21:36:41 +0800

    fix: remove BaseModel as functions are simple

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  include BaseModel
+  def json_attributes
+    attributes.clone
+  end
+
+  def print_errors
+    if errors.empty?
+      'No errors'
+    else
+      errors.full_messages.to_sentence
+    end
+  end
 end
