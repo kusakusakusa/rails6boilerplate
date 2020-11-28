fca1b32992e8931939d864312c6749be6cba5b60
Vic-L <vljc17@gmail.com>
2020-11-28 14:37:43 +0800

    fix: check against users that are not confirmable in devise

--- app/models/concerns/createable.rb
+++ app/models/concerns/createable.rb
@@ -11,7 +11,9 @@ module Createable
   class_methods do
     def create_with_temporary_password! other_attributes
       resource = self.new(other_attributes)
-      resource.skip_confirmation!
+      if resource.respond_to?(:confirmed?)
+        resource.skip_confirmation!
+      end
       resource.password = resource.temp_password
       resource.on_temporary_password = true
       resource.send("#{resource.temporary_password_change_flag}=", true)
@@ -21,14 +23,16 @@ module Createable
     end
   end
 
-  def on_temporary_password?
-    send(temporary_password_change_flag)
-  end
-
   def temp_password
     "#{Digest::MD5.hexdigest("#{email}#{Rails.env}")}"
   end
 
+  # overwrite if there's a different logic/attribute is not boolean
+  def on_temporary_password?
+    send(temporary_password_change_flag)
+  end
+
+  # overwrite if model using different attribute
   def temporary_password_change_flag
     "on_temporary_password"
   end
